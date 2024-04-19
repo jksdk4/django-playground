0f3c9d77aeb5858f0c4476ba3c9cd52c53c2c86c
Jasmine S <49571750+jksdk4@users.noreply.github.com>
2024-04-18 21:49:38 -0500

    update views to class-based
    so much cleaner!

--- smartnotes/notes/views.py
+++ smartnotes/notes/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+# from django.http import Http404
+from django.views.generic import DetailView, ListView
 
 # Create your views here.
 from .models import Notes
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+    
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+
+    ''' 
+    it's 'note' and not 'notes' because it's passing in a single
+    'note' into the notes_detail template
+    '''
+    
+# def detail(request, pk):
+#     try:
+#         note = Notes.objects.get(pk=pk)
+#     except:
+#         raise Http404("Note doesn't exist.")
+#     return render(request, 'notes/notes_detail.html', {'note': note})
